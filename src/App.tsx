ccc2b3832ec3cdd38be2f84d067587d00240a476
uzair.ajmal@blocksdev.com <MUzairAjml>
2023-01-27 16:46:24 +0500

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import '@client/App.scss';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <h1>My first Project</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
